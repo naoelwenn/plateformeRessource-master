b83c94049a34e6283227c914d6c175ad38441d60
nolwenn <nonomoc@hotmail.fr>
2025-04-21 12:05:49 +0200

    api ressources relationnelles

--- plateformeRessource-master/Backend/src/main/java/com/example/ressourceRelationnelle/RessourceRelationnelleApplication.java
+++ plateformeRessource-master/Backend/src/main/java/com/example/ressourceRelationnelle/RessourceRelationnelleApplication.java
@@ -3,13 +3,9 @@ package com.example.ressourceRelationnelle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class RessourceRelationnelleApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(RessourceRelationnelleApplication.class, args);
     }
-
-
 }
